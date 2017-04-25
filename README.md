be374f4cb4f7f088d6808f0129372057f520eade
David Binns <david.binns@geospock.com>
2017-04-25 15:15:49 +0100

    Supports Object.entries

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 `Object.values(obj)` -> `Object.keys(obj).map(key => obj[key])`
+
+`Object.entries(obj)` -> `Object.keys(obj).map(key => [key, obj[key]])`
+
+Forked from Jacob Mischka's https://github.com/jacobmischka/babel-plugin-object-values-to-object-keys
--- src/index.js
+++ src/index.js
@@ -1,42 +1,52 @@
 export default function({types: t}){
-	return {
-		visitor: {
-			CallExpression(path){
-				if(t.isMemberExpression(path.node.callee)
-						&& t.isIdentifier(path.node.callee.object, {name: 'Object'})
-						&& t.isIdentifier(path.node.callee.property, {name: 'values'})){
-					let obj = path.node.arguments[0];
+    return {
+        visitor: {
+            CallExpression(path){
+                if (t.isMemberExpression(path.node.callee) && t.isIdentifier(path.node.callee.object, {name: 'Object'})) {
+                    let isValues = t.isIdentifier(path.node.callee.property, {name: 'values'});
+                    let isEntries = t.isIdentifier(path.node.callee.property, {name: 'entries'});
 
-					path.replaceWith(
-						t.callExpression(
-							t.memberExpression(
-								t.callExpression(
-									t.memberExpression(
-										t.identifier('Object'),
-										t.identifier('keys')
-									),
-									[
-										obj
-									]
-								),
-								t.identifier('map')
-							),
-							[
-								t.arrowFunctionExpression(
-									[
-										t.identifier('key')
-									],
-									t.memberExpression(
-										obj,
-										t.identifier('key'),
-										true
-									)
-								)
-							]
-						)
-					);
-				}
-			}
-		}
-	};
+                    if (isValues || isEntries) {
+                        let target = path.node.arguments[0];
+
+                        let parameter = t.identifier('target');
+                        let key = t.identifier('key');
+                        let value = t.memberExpression(
+                            parameter,
+                            t.identifier('key'),
+                            true
+                        );
+
+                        let item = isEntries ? t.arrayExpression([key, value]) : value;
+
+                        path.replaceWith(
+                            t.callExpression(
+                                t.arrowFunctionExpression(
+                                    [parameter],
+                                    t.callExpression(
+                                        t.memberExpression(
+                                            t.callExpression(
+                                                t.memberExpression(
+                                                    t.identifier('Object'),
+                                                    t.identifier('keys')
+                                                ),
+                                                [
+                                                    parameter
+                                                ]
+                                            ),
+                                            t.identifier('map')
+                                        ),
+                                        [
+                                            t.arrowFunctionExpression([key], item)
+                                        ]
+                                    )
+                                ),
+                                [target]
+                            )
+                        );
+                    }
+                }
+            }
+        }
+    };
 }
